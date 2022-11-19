12a1bb5a019c7b37e9a047ea0b07770ebb3ce40f
Madhu Sudhan <m02sudhan@gmail.com>
2022-11-19 11:05:37 +0530

    Added : adding new contact

---
+++ AddNewContact.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddNewContact {
+    public static void main(String[] args) {
+
+        /*
+         * Starting with displaying welcome to address program
+         * */
+
+        System.out.println("Welcome to Address Book Program");
+        Contacts.createContact();
+    }
+
+
+    class Contacts {
+        /**creating contact using Arraylist
+         * taking input from console
+         * **/
+
+        static void createContact() {
+            ArrayList<String> firstContact = new ArrayList<String>(7);
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the contact details  : ");
+            System.out.println("Name Address City State Zipcode PhoneNo EmailId  : ");
+            for (int i = 0; i < 7; i++) {
+                String k = s.nextLine();
+                firstContact.add(k);
+            }
+            System.out.println(firstContact );
+        }
+    }
+}
